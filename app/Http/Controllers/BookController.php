8c4ef28e43af3b6865570349cd95423929f0fd04
dima.moisieienko <dimonmail09@gmail.com>
2026-08-01 14:34:50 +0300

    implemented first part of CRUD for Book including index, store, show

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBookRequest;
+use App\Http\Resources\BookResource;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $books = Book::with('author')->paginate(10);
+
+        return BookResource::collection($books);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBookRequest $request)
+    {
+        $book = Book::create($request->validated());
+
+        $book->load('author');
+
+        return new BookResource($book);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Book $book)
+    {
+        try {
+            $book->load('author');
+
+            return new BookResource($book);
+        }
+
+        catch(\Exception $th) {
+            return response()->json(['message' => 'Book not found'], 404);
+        }
+
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
