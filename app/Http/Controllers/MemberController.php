7b81f81683d6327b467fcbc9495231801b0b16a7
dima.moisieienko <dimonmail09@gmail.com>
2026-08-03 17:22:20 +0300

    implemented CRUD for Member

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreMemberRequest;
+use App\Http\Requests\UpdateMemberRequest;
+use App\Http\Resources\MemberResource;
+use App\Models\Member;
+use Illuminate\Http\Request;
+
+class MemberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $query = Member::with('activeBorrowings');
+
+        if($request->has('search')) {
+            $search = $request->search;
+
+            $query->where(function($q) use ($search) {
+                $q->where('name', 'like', '%'.$search.'%')
+                    ->orWhere('email', 'like', '%'.$search.'%');
+
+            });
+        }
+
+        if($request->has('status')) {
+            $query->where('status', $request->status);
+        }
+
+        //get members
+        $members = $query->paginate(10);
+
+        return MemberResource::collection($members);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMemberRequest $request)
+    {
+        $member = Member::create($request->validated());
+
+        return new MemberResource($member);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Member $member)
+    {
+        $member->load(['activeBorrowings', 'borrowings']);
+
+        return new MemberResource($member);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMemberRequest $request, Member $member)
+    {
+        $member->update($request->validated());
+
+        return new MemberResource($member);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Member $member)
+    {
+        //check if member has any active borrowings
+
+        if($member->activeBorrowings()->count() > 0) {
+            return response()->json([
+                'message' => 'Member cannot be deleted because it has borrowed books.'
+            ], 422);
+        }
+
+        $member->delete();
+
+        return response()->json([
+            'message' => 'Member successfully deleted.'
+        ]);
+    }
+}
--- app/Http/Requests/UpdateBookRequest.php
+++ app/Http/Requests/UpdateBookRequest.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-class StoreBookRequest extends FormRequest
+class UpdateBookRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -25,7 +25,9 @@ class StoreBookRequest extends FormRequest
     {
         return [
             'title' => 'required|string|max:255',
-            'isbn' => ['required','string','unique:books,isbn'],
+            'isbn' => ['required','string',
+                Rule::unique('books', 'isbn') ->ignore($this->route('book')->id)
+            ],
             'description' => 'nullable|string',
             'author_id' => 'required|exists:authors,id',
             'genre' => 'nullable|string',
